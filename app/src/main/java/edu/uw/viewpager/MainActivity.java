6ef2156987d5411d6d924492a9df76b958c7586f
Wuyi Zhang <wuyi_zhang@outlook.com>
2017-10-16 21:26:09 -0700

    Finished Lab 03 - ViewPager

--- app/src/main/java/edu/uw/viewpager/MainActivity.java
+++ app/src/main/java/edu/uw/viewpager/MainActivity.java
@@ -1,43 +1,101 @@
 package edu.uw.viewpager;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements MovieListFragment.OnMovieSelectedListener {
+public class MainActivity extends AppCompatActivity implements
+        SearchFragment.OnSearchListener,
+        MovieListFragment.OnMovieSelectedListener {
 
     private static final String TAG = "MainActivity";
-    public static final String MOVIE_LIST_FRAGMENT_TAG = "MoviesListFragment";
-    public static final String MOVIE_DETAIL_FRAGMENT_TAG = "DetailFragment";
+
+    private SearchFragment searchFragment;
+    private MovieListFragment movieListFragment;
+    private DetailFragment detailFragment;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchFragment = searchFragment.newInstance();
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MoviePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
     }
 
-    //respond to search button clicking
-    public void handleSearchClick(View v){
-        EditText text = (EditText)findViewById(R.id.txt_search);
-        String searchTerm = text.getText().toString();
-
-        MovieListFragment fragment = MovieListFragment.newInstance(searchTerm);
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragment, MOVIE_LIST_FRAGMENT_TAG);
-        ft.addToBackStack(null);
-        ft.commit();
+    @Override
+    public void onSearchSubmitted(String searchTerm) {
+        movieListFragment = MovieListFragment.newInstance(searchTerm);
+        pagerAdapter.notifyDataSetChanged();
+        viewPager.setCurrentItem(1);
     }
 
     @Override
     public void onMovieSelected(Movie movie) {
-        DetailFragment fragment = DetailFragment.newInstance(movie);
+        Log.v(TAG, "Detail for " + movie);
+        detailFragment = DetailFragment.newInstance(movie);
+        pagerAdapter.notifyDataSetChanged();
+        viewPager.setCurrentItem(2);
+    }
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragment, MOVIE_DETAIL_FRAGMENT_TAG);
-        ft.addToBackStack(null); //remember for the back button
-        ft.commit();
+    private class MoviePagerAdapter extends FragmentStatePagerAdapter {
+
+        public MoviePagerAdapter (FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return searchFragment;
+                case 1:
+                    return movieListFragment;
+                case 2:
+                    return detailFragment;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            if (movieListFragment == null) {
+                // before search, movie list is null
+                return 1;
+            } else if (detailFragment == null) {
+                // before selection, detail is null
+                return 2;
+            } else {
+                return 3;
+            }
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
     }
 }
